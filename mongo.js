ed11c63353332196db3d8c19ce7ad7ab7cdf32f7
launis <62068629+launis@users.noreply.github.com>
2021-12-13 09:17:52 +0200

    3.12

--- mongo.js
+++ mongo.js
@@ -6,32 +6,51 @@ if (process.argv.length<3) {
   process.exit(1)
 }
 
+if (process.argv.length===4) {
+  console.log('give both name and number')
+  process.exit(1)
+}
+
+if (process.argv.length>5) {
+  console.log('too many arguments')
+  process.exit(1)
+}
+
 const password = process.argv[2]
-
-console.log(password)
-
 const url =
   `mongodb+srv://fullstack:${password}@cluster0.cnunu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 mongoose.connect(url)
-
-
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
   id: Number,
 })
-
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    name: "Juho Kustinen", 
-    number: "040-123456",
-    id: 1,
-})
 
+const generateId = () => {
+  const id = Math.round(Math.random() * 1000)
+  return id
+}
 
-person.save().then(response => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+if (process.argv.length===3) {
+  console.log(`phonebook:`)
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+else {
+  const person = new Person({
+    name: process.argv[3], 
+    number: process.argv[4],
+    id: generateId(),
+  })
+  person.save().then(response => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
